c707778eabd7fe6d177d4eb9379ff51838f2799b
David Tetzlaff <davetz@outlook.com>
2020-03-05 15:34:13 -0600

    Begin refactor for custom tables

--- ModelGraph/ModelGraph.Control/Controller.cs
+++ ModelGraph/ModelGraph.Control/Controller.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ModelGraph.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace ModelGraph.Control
 {
